3d789292e923cf1a3c884ff249dd19a5f1c4967c
Prateek Surana <prateeksurana3255@gmail.com>
2022-01-23 23:11:30 +0530

    💡 Remove unused comments

--- src/extension.ts
+++ src/extension.ts
@@ -2,8 +2,6 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('multiple-cursor-same-case.matchCaseCursor', () => {
 		vscode.commands.executeCommand('toggleFindCaseSensitive');
 		vscode.commands.executeCommand('editor.action.addSelectionToNextFindMatch');
